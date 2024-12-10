5d855ab4012c6eb76cf8e41bfa6c74e26656a853
nupur dubey <nupur.dubey@apptware.com>
2024-12-10 16:23:01 +0530

    added  some test cases and updated commands.js

--- cypress/e2e/category.js
+++ cypress/e2e/category.js
@@ -2,152 +2,144 @@
 //import 'cypress-file-upload';
 describe('Admin Dashboard scenarios', () => {
     beforeEach(() => {
-     cy.visit('https://qa-admin.pclive247.com/')
-     cy.contains('LOGIN').should('be.visible')
-     cy.login("tester@admin.com","Admin@123")
-     cy.contains('Category').click()
-     })
- 
-     it('ADD category with valid details',{ pageLoadTimeout: 120000 },() =>{
-         cy.contains('Add category').click()
-         cy.get('[placeholder="Title"]').click().type('Marvel')
-         cy.get('[placeholder="Description"]').click().type('This category will include all Marvel(raw/slab) cards')
-         cy.get('input[type="file"]').selectFile('C:\\Users\\Lenovo\\Desktop\\PCLive247\\Baseball\\Baseball 1.jpg',{force: true})
-         cy.get('[type="button"]').eq(17).click()
-         //cy.get(':nth-child(2) > .text-right > .MuiButton-contained')
-              
+        cy.visit('https://qa-admin.pclive247.com/')
+        cy.contains('LOGIN').should('be.visible')
+        cy.login("tester@admin.com", "Admin@123")
+        cy.contains('Category').click()
     })
-     it ('ADD category with existing details details',{ pageLoadTimeout: 120000 },() =>{
-         
-         cy.contains('Add category').click()
-         cy.get('[placeholder="Title"]').click().type('CSK')
-         cy.get('[placeholder="Description"]').click().type('This category will include all Marvel(raw/slab) cards')
-         cy.get('[type="button"]').eq(17).click()
-         cy.contains('A category with this name already exists.').should('be.visible')
-     })
- 
-     it('Check validations while adding category',{ pageLoadTimeout: 120000 },() => {
-         cy.contains('Add category').click()
-         cy.get('[placeholder="Title"]').click().type('test')
-         cy.get('[type="button"]').eq(17).click()
-         cy.contains('Please enter description')
-         /// verify when title kept empty
-         cy.get('[placeholder="Title"]').click().clear()
-         cy.get('[placeholder="Description"]').click().type('This category will include all Marvel(raw/slab) cards')
-         cy.get('[type="button"]').eq(17).click()
-         cy.contains('Please enter title').should('be.visible')
-         /// Verify when incorrect image got upload
-         cy.get('[placeholder="Title"]').click().type('Marvel')
-         cy.get('[placeholder="Description"]').click().type('This category will include all Marvel(raw/slab) cards')
-         //incorrect filepath will lead to failed TC
-        //  cy.get('input[type="file"]').selectFile('C:\\Users\\Lenovo\\Desktop\\PCLive247\\Baseball\\Baseball123.png',{force: true})
-        //  cy.get('[type="button"]').eq(17).click()
-         
-     })
-     it('Verify Cancel button working',{ pageLoadTimeout: 120000 },() => {
-         cy.contains('Add category').click()
-         cy.get('[type="button"]').eq(18).click()
-                 
-     })
- 
-     it('Sorting of Name column when column header is clicked', () => {
-         // Get the table column header and click to sort (e.g., sorting by 'Name' column)
-         //cy.get('[class="sort-icon"]').eq(0).click();
-         cy.wait(2000);
-         cy.get('tr').find('td').first().invoke('text').then((expectedValue)=>{
-         console.log(expectedValue)
-         cy.get('[class="sort-icon"]').eq(0).click({force:true});
-         cy.wait(2000);
-         cy.get('tr').find('td').first().invoke('text').then((actualValue)=>{
-             console.log(actualValue)
-             expect(actualValue).to.not.equal(expectedValue);
-             // Assert that the table is correctly sorted in ascending order
-             expect(actualValue.substring(0, 1)).to.equal('A');
-                 
-         })
- 
- 
-     })
- 
-       });
-       it ('Sorting of Number of orders',()=>{
-         cy.wait(2000);
-         cy.get('tr').find('td').eq(3).invoke('text').then((expectedValue)=>{
-         console.log(expectedValue)
-         cy.get('[class="sort-icon"]').eq(1).click({force:true});
-         cy.wait(2000);
-         cy.get('tr').find('td').eq(3).invoke('text').then((actualValue)=>{
-             console.log(actualValue)
-             expect(actualValue).to.equal(expectedValue);
-             // Assert that the table is correctly sorted in ascending order
-             expect(actualValue.substring(0, 1)).to.equal('0');})
-            })
-           })
-     //Actions on category
-     it('Edit actions on category',()=>{
-         cy.get('[data-testid="EditIcon"]').eq(0).click()
-         cy.get('[id="input-postal-code"]').clear().type('Marvel Cards')
-         cy.get('[placeholder="Description"]').clear().type('ALL Marvel cards')
-         cy.get('[data-testid="CancelIcon"]').click()
-         cy.get('input[type="file"]').selectFile('C:\\Users\\Lenovo\\Desktop\\PCLive247\\Marvel-CaptainAmerica.webp',{force: true})
-         cy.contains('Update').click()
-         cy.contains('Category updated successfully !!').should('be.visible')
-         // Verify 'Cancel' button is clickable
-         cy.get('[data-testid="EditIcon"]').eq(0).click()
-         cy.contains('Cancel').click()
-     })
- 
-     it('Inactivate category which is not used by any cards',()=>{
-         cy.wait(2000)
-         cy.get('[type="checkbox"]').eq(0).click({force: true})
-         cy.contains('Category deactivated successfully !!').should('be.visible')
-         cy.wait(2000)
-         cy.get('[type="checkbox"]').eq(0).click({force: true})
-         cy.contains('Category activated successfully !!').should('be.visible')
-     })
-     it('Delete category with no cards',()=>{
-         cy.wait(2000)
-         cy.get(':nth-child(1) > :nth-child(5) > .actionIconsContainer > [data-testid="DeleteIcon"] > path', { timeout: 10000 })
-          .click({ force: true });
-         cy.get('[type="button"]').contains('Yes').click()
-         cy.contains('Category deleted successfully !!').should('be.visible')
-     })
-  // Filters working
-     it('Filters - category status',()=>{
-        cy.wait(5000)
-         cy.get('[class="multilevel-dropdown-container"]').click()
-         //cy.contains('Status').click()
-         cy.get('[class="Item_item__azkIp"]').realHover()
-         // Active Category 
-         cy.get('[class="MuiTypography-root MuiTypography-body1 MuiFormControlLabel-label css-9l3uo3"]').eq(0).click()
-         cy.get('[class="apply"]').click()
-         // Inactive Category
-         cy.get('[class="MuiTypography-root MuiTypography-body1 MuiFormControlLabel-label css-9l3uo3"]').eq(0).click()         // Active Category 
-         cy.get('[class="MuiTypography-root MuiTypography-body1 MuiFormControlLabel-label css-9l3uo3"]').eq(1).click()
-         cy.get('[class="apply"]').click()
-         cy.contains('Reset All').click()
-         
-     })
 
-     //Search category with valid data
-     it('Search category with valid data',()=>{
- 
+    it('ADD category with valid details', { pageLoadTimeout: 120000 }, () => {
+        cy.contains('Add category').click()
+        cy.get('[placeholder="Title"]').click().type('Marvel')
+        cy.get('[placeholder="Description"]').click().type('This category will include all Marvel(raw/slab) cards')
+        cy.get('input[type="file"]').selectFile('C:\\Users\\Lenovo\\Desktop\\PCLive247\\Baseball\\Baseball 1.jpg', { force: true })
+        cy.get('[type="button"]').eq(17).click()
+
+    })
+    it('ADD category with existing details details', { pageLoadTimeout: 120000 }, () => {
+
+        cy.contains('Add category').click()
+        cy.get('[placeholder="Title"]').click().type('CSK')
+        cy.get('[placeholder="Description"]').click().type('This category will include all Marvel(raw/slab) cards')
+        cy.get('[type="button"]').eq(17).click()
+        cy.contains('A category with this name already exists.').should('be.visible')
+    })
+
+    it('Check validations while adding category', { pageLoadTimeout: 120000 }, () => {
+        cy.contains('Add category').click()
+        cy.get('[placeholder="Title"]').click().type('test')
+        cy.get('[type="button"]').eq(17).click()
+        cy.contains('Please enter description')
+        /// verify when title kept empty
+        cy.get('[placeholder="Title"]').click().clear()
+        cy.get('[placeholder="Description"]').click().type('This category will include all Marvel(raw/slab) cards')
+        cy.get('[type="button"]').eq(17).click()
+        cy.contains('Please enter title').should('be.visible')
+        /// Verify when incorrect image got upload
+        cy.get('[placeholder="Title"]').click().type('Marvel')
+        cy.get('[placeholder="Description"]').click().type('This category will include all Marvel(raw/slab) cards')
+        //incorrect filepath will lead to failed TC
+    })
+    it('Verify Cancel button working', { pageLoadTimeout: 120000 }, () => {
+        cy.contains('Add category').click()
+        cy.get('[type="button"]').eq(18).click()
+
+    })
+
+    it('Sorting of Name column when column header is clicked', () => {
+        // Get the table column header and click to sort (e.g., sorting by 'Name' column)
+        cy.wait(2000);
+        cy.get('tr').find('td').first().invoke('text').then((expectedValue) => {
+            console.log(expectedValue)
+            cy.get('[class="sort-icon"]').eq(0).click({ force: true });
+            cy.wait(2000);
+            cy.get('tr').find('td').first().invoke('text').then((actualValue) => {
+                console.log(actualValue)
+                expect(actualValue).to.not.equal(expectedValue);
+                // Assert that the table is correctly sorted in ascending order
+                expect(actualValue.substring(0, 1)).to.equal('A');
+
+            })
+
+
+        })
+
+    });
+    it('Sorting of Number of orders', () => {
+        cy.wait(2000);
+        cy.get('tr').find('td').eq(3).invoke('text').then((expectedValue) => {
+            console.log(expectedValue)
+            cy.get('[class="sort-icon"]').eq(1).click({ force: true });
+            cy.wait(2000);
+            cy.get('tr').find('td').eq(3).invoke('text').then((actualValue) => {
+                console.log(actualValue)
+                expect(actualValue).to.equal(expectedValue);
+                // Assert that the table is correctly sorted in ascending order
+                expect(actualValue.substring(0, 1)).to.equal('0');
+            })
+        })
+    })
+    //Actions on category
+    it('Edit actions on category', () => {
+        cy.get('[data-testid="EditIcon"]').eq(0).click()
+        cy.get('[id="input-postal-code"]').clear().type('Marvel Cards')
+        cy.get('[placeholder="Description"]').clear().type('ALL Marvel cards')
+        cy.get('[data-testid="CancelIcon"]').click()
+        cy.get('input[type="file"]').selectFile('C:\\Users\\Lenovo\\Desktop\\PCLive247\\Marvel-CaptainAmerica.webp', { force: true })
+        cy.contains('Update').click()
+        cy.contains('Category updated successfully !!').should('be.visible')
+        // Verify 'Cancel' button is clickable
+        cy.get('[data-testid="EditIcon"]').eq(0).click()
+        cy.contains('Cancel').click()
+    })
+
+    it('Inactivate category which is not used by any cards', () => {
+        cy.wait(2000)
+        cy.get('[type="checkbox"]').eq(0).click({ force: true })
+        cy.contains('Category deactivated successfully !!').should('be.visible')
+        cy.wait(2000)
+        cy.get('[type="checkbox"]').eq(0).click({ force: true })
+        cy.contains('Category activated successfully !!').should('be.visible')
+    })
+    it('Delete category with no cards', () => {
+        cy.wait(2000)
+        cy.get(':nth-child(1) > :nth-child(5) > .actionIconsContainer > [data-testid="DeleteIcon"] > path', { timeout: 10000 })
+            .click({ force: true });
+        cy.get('[type="button"]').contains('Yes').click()
+        cy.contains('Category deleted successfully !!').should('be.visible')
+    })
+    // Filters working
+    it('Filters - category status', () => {
+        cy.wait(5000)
+        cy.get('[class="multilevel-dropdown-container"]').click()
+      cy.get('[class="Item_item__azkIp"]').realHover()
+        // Active Category 
+        cy.get('[class="MuiTypography-root MuiTypography-body1 MuiFormControlLabel-label css-9l3uo3"]').eq(0).click()
+        cy.get('[class="apply"]').click()
+        // Inactive Category
+        cy.get('[class="MuiTypography-root MuiTypography-body1 MuiFormControlLabel-label css-9l3uo3"]').eq(0).click()         // Active Category 
+        cy.get('[class="MuiTypography-root MuiTypography-body1 MuiFormControlLabel-label css-9l3uo3"]').eq(1).click()
+        cy.get('[class="apply"]').click()
+        cy.contains('Reset All').click()
+
+    })
+
+    //Search category with valid data
+    it('Search category with valid data', () => {
         cy.wait(2000)
         cy.get('[type="text"]').type('CSK')
-        cy.get('[id="Search"]').click({force: true})
+        cy.get('[id="Search"]').click({ force: true })
         cy.contains('Reset All').click()
-     })
-     //Search category with invalid data
-     it.only('Search category with valid data',()=>{
- 
+    })
+    //Search category with invalid data
+    it('Search category with valid data', () => {
         cy.wait(2000)
         cy.get('[type="text"]').type('Accesories')
-        cy.get('[id="Search"]').click({force: true})
+        cy.get('[id="Search"]').click({ force: true })
         cy.contains('No data found!').should('be.visible')
         cy.contains('Reset All').click()
-     })
- 
- })
-       
-     
- 
+    })
+
+})
+
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,20 +27,117 @@
 ///< referance types ="Cypress"/>
 require("cypress-real-events")
 Cypress.Commands.add("login", () => {
-    cy.get('[type="email"]').type('tester@admin.com')
-         cy.get('[type="password"]').type('Admin@123')
-         cy.get('[type="button"]').contains('Sign in').click()
-         cy.contains('OTP sent on tester@admin.com').should('be.visible')
-         cy.contains('Verify account').should('be.visible')
-         const values = ['1', '2', '3', '4', '5', '6'];
-         cy.get('[inputmode="text"]').each(($input, index) => {
-         cy.wrap($input).type(values[index]);
-    });
-    cy.get('[type="button"]').contains('Submit').click()
-  });
+   cy.get('[type="email"]').type('tester@admin.com')
+   cy.get('[type="password"]').type('Admin@123')
+   cy.get('[type="button"]').contains('Sign in').click()
+   cy.contains('OTP sent on tester@admin.com').should('be.visible')
+   cy.contains('Verify account').should('be.visible')
+   const values = ['1', '2', '3', '4', '5', '6'];
+   cy.get('[inputmode="text"]').each(($input, index) => {
+      cy.wrap($input).type(values[index]);
+   });
+   cy.get('[type="button"]').contains('Submit').click()
+});
 
-  Cypress.Commands.add('logout', ()=>{
-     cy.get('.align-items-center.media').last().click({force:true})
-     cy.get('.dropdown-menu-arrow').contains('Logout').click({force:true})
-     cy.contains('LOGIN').should('exist')
-  })
+Cypress.Commands.add('logout', () => {
+   cy.get('.align-items-center.media').last().click({ force: true })
+   cy.get('.dropdown-menu-arrow').contains('Logout').click({ force: true })
+   cy.contains('LOGIN').should('exist')
+})
+// Corporate Module - Searching functionality
+Cypress.Commands.add('search', () => {
+   cy.wait(2000)
+   cy.get('[id="outlined-size-small"]').click().type('nupur')
+   cy.get('[class="MuiButtonBase-root MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium MuiButton-colorPrimary MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium MuiButton-colorPrimary search-action css-j9olmy"]')
+      .click()
+   cy.get('[class="table-responsive"]').eq(1).then(() => {
+      if (cy.contains('nupur')) {
+         cy.contains('Reset All').click()
+      } else {
+         cy.contains('No data found!').should('be.visible')
+      }
+   })
+   cy.contains('Reset All').click()
+   cy.get('[id="outlined-size-small"]').click().type('Myntra')
+   cy.get('.search-input-container > .MuiButton-root').click()
+   cy.contains('No data found!').should('be.visible')
+})
+
+
+// Corporate Module - Sorting
+Cypress.Commands.add('sorting', () => {
+   cy.wait(2000)
+   //sort ID column in ascending order
+   cy.get('[class="sort-icon"]').click()
+   //sort ID column in descending order
+   cy.get('[class="sort-icon-dark"]').click()
+   //Sort Updated at in ascending order 
+   cy.get('[class="sort-icon"]').click()
+   //sort Updated at in descending order
+   cy.get('[class="sort-icon-dark"]').click
+})
+
+Cypress.Commands.add('actions', () => {
+   cy.wait(2000)
+   //Approve request
+   cy.get('[class="buttons-container"]').eq(3).contains('Approve').click({ force: true })
+   cy.get('[class="text-center  MuiBox-root css-0"]').contains('Yes, approve request').click()
+   cy.contains('Request state updated successfully').should('be.visible')
+   cy.get('[class="buttons-container"]').eq(3).contains('Decline').click({ force: true })
+   cy.get('[class="text-center  MuiBox-root css-0"]').contains('Yes, decline request').click()
+   cy.contains('Request state updated successfully').should('be.visible')
+   cy.wait(2000)
+   // cy.get('[data-testid="MoreVertIcon"]').scrollIntoView()
+   // cy.get('[data-testid="MoreVertIcon"]').eq(3).click({ fource: true })
+   // //[class="shadow card"]
+   // cy.contains('History').click()
+
+})
+Cypress.Commands.add('filters', () => {
+   cy.get('[class="calender-container"]').click()
+   // Filter by upload type 
+   cy.get('[class="Dropdown_dropdown__ZbOaq"]').contains('Upload Type').realHover()
+   // Select Normal upload
+   cy.contains('Normal').click({ force: true })
+   cy.get('[class="apply"]').contains('Apply').click({ force: true })
+   const commonText = 'NORMAL';
+   // Loop through the first 10 rows (or up to the actual number of rows)
+   cy.get('[class="table-responsive"]').eq(1).each(($row, index) => {
+      // Stop after 10 rows
+      if (index = 10) {
+         cy.get('[data-testid="NavigateNextIcon"]').click()
+         return false; // Break the loop after 10 iterations
+      }
+      // Find the 3rd column (City) in the current row and check if it matches the common text
+      cy.wrap($row).find('td').eq(2)  // Index 2 corresponds to the 'Upload type' column
+         .should('have.text', commonText);  // Check if the column text is 'Normal'
+   });
+   //cy.wait(5000)
+   cy.get('[class="calender-container"]').click()
+   cy.get('[class="Dropdown_dropdown__ZbOaq"]').contains('Upload Type').realHover()
+   // Select Bulk upload
+   cy.contains('Normal').click({ fource: true })
+   cy.contains('Bulk').click()
+   cy.get('[class="apply"]').contains('Apply').click()
+   cy.get('[class="Dropdown_dropdown__ZbOaq"]').contains('Upload Type').realHover()
+   // Select CSV upload
+   cy.contains('Bulk').click()
+   cy.contains('CSV').click()
+   cy.get('[class="apply"]').contains('Apply').click()
+})
+
+Cypress.Commands.add('addressfilters', () => {
+   cy.wait(2000)
+   cy.get('[class="Dropdown_dropdown__ZbOaq"]').contains('Pickup Address').realHover()
+   cy.contains('71 Saint Nicholas Dr, North Pole, 99705').click()
+   cy.get('[class="apply"]').contains('Apply').click({ force: true })
+   cy.contains('Reset All').click()
+})
+
+Cypress.Commands.add('moreHistory', () => {
+   cy.wait(2000)
+   cy.get('table tr:first-child svg[data-testid="MoreVertIcon"]').should('be.exist').click({ force: true })
+   cy.get('.actionButton ').contains('History').should('be.exist').click({ force: true })
+   cy.wait(2000)
+   cy.get('[data-testid="ArrowBackIcon"]').click()
+})
